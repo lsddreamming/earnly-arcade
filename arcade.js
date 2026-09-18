7f4c5d4f3498ee9a678621e61875d3c6fa98ba7d
lsddreamming <jdasch92@yahoo.com>
2026-09-18 12:17:56 -0500

    Add Tap Rush to daily play system

--- arcade.js
+++ arcade.js
@@ -1,6 +1,6 @@
 // Earnly prototype storage only. No real ad SDK or cash-out system is connected yet.
 const Arcade = (() => {
-  const names = { snake: 'Snake', blockDrop: 'Block Drop' };
+  const names = { snake: 'Snake', blockDrop: 'Block Drop', tapRush: 'Tap Rush' };
   const FREE_PLAYS = 3;
   const PLAY_AD_BONUS = 3;
   const COIN_AD_REWARD = 10;
