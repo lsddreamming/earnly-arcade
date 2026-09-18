85f5153d3628cb311324a030172d02940ffc78e2
lsddreamming <jdasch92@yahoo.com>
2026-09-18 13:18:53 -0500

    Add offline app shell service worker

---
+++ service-worker.js
@@ -0,0 +1,75 @@
+const CACHE_NAME = 'earnly-arcade-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './games.html',
+  './rewards.html',
+  './profile.html',
+  './stats.html',
+  './snake.html',
+  './blockdrop.html',
+  './taprush.html',
+  './memory.html',
+  './dodger.html',
+  './brickbreaker.html',
+  './junglehopper.html',
+  './towerstack.html',
+  './arcade.css',
+  './arcade.js',
+  './manifest.webmanifest',
+  './earnly-icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      Promise.allSettled(CORE_ASSETS.map(asset => cache.add(asset)))
+    ).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+          return response;
+        })
+        .catch(async () => {
+          const cached = await caches.match(request);
+          return cached || caches.match('./index.html');
+        })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then(cached => {
+      const network = fetch(request).then(response => {
+        if (response && response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+        }
+        return response;
+      }).catch(() => cached);
+
+      return cached || network;
+    })
+  );
+});
