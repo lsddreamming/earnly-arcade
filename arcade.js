a61df1d2f286d65b01b525df5dc6ca171a05b9da
lsddreamming <jdasch92@yahoo.com>
2026-09-18 16:23:38 -0500

    Prepare local data for cloud saves

--- arcade.js
+++ arcade.js
@@ -31,7 +31,7 @@ const Arcade = (() => {
   const ACHIEVEMENT_XP = 25;
   const WEEKLY_ALL_CLEAR_XP = 100;
   const DATA_SCHEMA_VERSION = 1;
-  const APP_VERSION = '0.14.4';
+  const APP_VERSION = '0.15.0';
 
   const streakRewardDefinitions = [
     { days:3, icon:'🔥', title:'3-Day Streak', rewardXP:25 },
@@ -209,7 +209,9 @@ const Arcade = (() => {
     'arcadeInstalled',
     'arcadeOnboardingSeen',
     'arcadeTransferDismissed',
-    'arcadeTransferRestored'
+    'arcadeTransferRestored',
+    'arcadeLastCloudSave',
+    'arcadeLastCloudRestore'
   ]);
 
   function snapshotData() {
