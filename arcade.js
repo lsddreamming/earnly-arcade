2f385f7b25ea6918f46f4f8bbc36002ba028c52c
lsddreamming <jdasch92@yahoo.com>
2026-09-18 15:32:37 -0500

    Bump prototype version to 0.13.5

--- arcade.js
+++ arcade.js
@@ -31,7 +31,7 @@ const Arcade = (() => {
   const ACHIEVEMENT_XP = 25;
   const WEEKLY_ALL_CLEAR_XP = 100;
   const DATA_SCHEMA_VERSION = 1;
-  const APP_VERSION = '0.13.4';
+  const APP_VERSION = '0.13.5';
 
   const streakRewardDefinitions = [
     { days:3, icon:'🔥', title:'3-Day Streak', rewardXP:25 },
