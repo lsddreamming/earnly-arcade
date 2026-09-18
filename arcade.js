7a4b1d578e8bcaef57db7041492abd86ffdc1264
lsddreamming <jdasch92@yahoo.com>
2026-09-18 18:42:06 -0500

    Make missions open their games

--- arcade.js
+++ arcade.js
@@ -31,7 +31,7 @@ const Arcade = (() => {
   const ACHIEVEMENT_XP = 25;
   const WEEKLY_ALL_CLEAR_XP = 100;
   const DATA_SCHEMA_VERSION = 1;
-  const APP_VERSION = '0.15.14';
+  const APP_VERSION = '0.15.15';
 
   const streakRewardDefinitions = [
     { days:3, icon:'🔥', title:'3-Day Streak', rewardXP:25 },
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'earnly-arcade-v50';
+const CACHE_NAME = 'earnly-arcade-v51';
 const CORE_ASSETS = [
   './',
   './index.html',
