cbcff50a164d75065c0f0e7df07d6db6a45c66d5
lsddreamming <jdasch92@yahoo.com>
2026-09-18 19:59:16 -0500

    Add sync visibility and tester diagnostics

--- arcade.js
+++ arcade.js
@@ -31,7 +31,7 @@ const Arcade = (() => {
   const ACHIEVEMENT_XP = 25;
   const WEEKLY_ALL_CLEAR_XP = 100;
   const DATA_SCHEMA_VERSION = 1;
-  const APP_VERSION = '0.15.26';
+  const APP_VERSION = '0.15.27';
 
   const streakRewardDefinitions = [
     { days:3, icon:'🔥', title:'3-Day Streak', rewardXP:25 },
@@ -2424,6 +2424,7 @@ const Arcade = (() => {
       textScale:textScale(),
       textScalePercent:textScalePercent(),
       install:installStatus(),
+      deviceId:deviceId(),
       sync:syncStatus()
     };
   }
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'earnly-arcade-v62';
+const CACHE_NAME = 'earnly-arcade-v63';
 const CORE_ASSETS = [
   './',
   './index.html',
