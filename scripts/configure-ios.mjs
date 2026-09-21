30fef25ec79cb410ffc645ae3e593a2a88d08639
lsddreamming <jdasch92@yahoo.com>
2026-09-21 11:27:11 -0500

    Expand iOS SKAdNetwork configuration

--- scripts/configure-ios.mjs
+++ scripts/configure-ios.mjs
@@ -7,36 +7,96 @@ const appId = testMode
   ? 'ca-app-pub-3940256099942544~1458002511'
   : 'ca-app-pub-8864401806510610~7658950353';
 
+// Google Mobile Ads SKAdNetwork buyer list. Refresh this list from Google's
+// iOS privacy strategy documentation before major releases.
+const skAdNetworkIds = [
+  "cstr6suwn9.skadnetwork",
+  "4fzdc2evr5.skadnetwork",
+  "2fnua5tdw4.skadnetwork",
+  "ydx93a7ass.skadnetwork",
+  "p78axxw29g.skadnetwork",
+  "v72qych5uu.skadnetwork",
+  "ludvb6z3bs.skadnetwork",
+  "cp8zw746q7.skadnetwork",
+  "3sh42y64q3.skadnetwork",
+  "c6k4g5qg8m.skadnetwork",
+  "s39g8k73mm.skadnetwork",
+  "wg4vff78zm.skadnetwork",
+  "3qy4746246.skadnetwork",
+  "f38h382jlk.skadnetwork",
+  "hs6bdukanm.skadnetwork",
+  "mlmmfzh3r3.skadnetwork",
+  "v4nxqhlyqp.skadnetwork",
+  "wzmmz9fp6w.skadnetwork",
+  "su67r6k2v3.skadnetwork",
+  "yclnxrl5pm.skadnetwork",
+  "t38b2kh725.skadnetwork",
+  "7ug5zh24hu.skadnetwork",
+  "gta9lk7p23.skadnetwork",
+  "vutu7akeur.skadnetwork",
+  "y5ghdn5j9k.skadnetwork",
+  "v9wttpbfk9.skadnetwork",
+  "n38lu8286q.skadnetwork",
+  "47vhws6wlr.skadnetwork",
+  "kbd757ywx3.skadnetwork",
+  "9t245vhmpl.skadnetwork",
+  "a2p9lx4jpn.skadnetwork",
+  "22mmun2rn5.skadnetwork",
+  "44jx6755aq.skadnetwork",
+  "k674qkevps.skadnetwork",
+  "4468km3ulz.skadnetwork",
+  "2u9pt9hc89.skadnetwork",
+  "8s468mfl3y.skadnetwork",
+  "klf5c3l5u5.skadnetwork",
+  "ppxm28t8ap.skadnetwork",
+  "kbmxgpxpgc.skadnetwork",
+  "uw77j35x4d.skadnetwork",
+  "578prtvx9j.skadnetwork",
+  "4dzt52r2t5.skadnetwork",
+  "tl55sbb4fm.skadnetwork",
+  "c3frkrj4fj.skadnetwork",
+  "e5fvkxwrpn.skadnetwork",
+  "8c4e2ghe7u.skadnetwork",
+  "3rd42ekr43.skadnetwork",
+  "97r2b46745.skadnetwork",
+  "3qcr597p9d.skadnetwork"
+];
+
 let plist = await readFile(plistPath, 'utf8');
 
 const trackingUsageRegex = /\s*<key>NSUserTrackingUsageDescription<\/key>\s*<string>[^<]*<\/string>/g;
 plist = plist.replace(trackingUsageRegex, '');
 
-const keys = [
-  '<key>GADApplicationIdentifier</key>',
-  '<key>SKAdNetworkItems</key>'
-];
+const gadApplicationRegex = /\s*<key>GADApplicationIdentifier<\/key>\s*<string>[^<]*<\/string>/g;
+const skAdNetworkRegex = /\s*<key>SKAdNetworkItems<\/key>\s*<array>[\s\S]*?<\/array>/g;
+plist = plist
+  .replace(gadApplicationRegex, '')
+  .replace(skAdNetworkRegex, '');
 
-if (!keys.some(key => plist.includes(key))) {
-  const block = [
-    '  <key>GADApplicationIdentifier</key>',
-    '  <string>' + appId + '</string>',
-    '  <key>SKAdNetworkItems</key>',
-    '  <array>',
+const skAdNetworkBlock = [
+  '  <key>GADApplicationIdentifier</key>',
+  '  <string>' + appId + '</string>',
+  '  <key>SKAdNetworkItems</key>',
+  '  <array>',
+  ...skAdNetworkIds.flatMap(id => [
     '    <dict>',
     '      <key>SKAdNetworkIdentifier</key>',
-    '      <string>cstr6suwn9.skadnetwork</string>',
-    '    </dict>',
-    '  </array>'
-  ].join('\n');
+    '      <string>' + id + '</string>',
+    '    </dict>'
+  ]),
+  '  </array>'
+].join('\n');
 
-  plist = plist.replace(/\n<\/dict>\s*<\/plist>\s*$/, '\n' + block + '\n</dict>\n</plist>\n');
-} else {
-  plist = plist.replace(
-    /(<key>GADApplicationIdentifier<\/key>\s*<string>)[^<]*(<\/string>)/,
-    '$1' + appId + '$2'
-  );
-}
+plist = plist.replace(
+  /\n<\/dict>\s*<\/plist>\s*$/,
+  '\n' + skAdNetworkBlock + '\n</dict>\n</plist>\n'
+);
 
 await writeFile(plistPath, plist);
-console.log('Configured iOS AdMob app ID in ' + (testMode ? 'TEST' : 'LIVE') + ' mode');
+console.log(
+  'Configured iOS AdMob app ID in ' +
+  (testMode ? 'TEST' : 'LIVE') +
+  ' mode with ' +
+  skAdNetworkIds.length +
+  ' SKAdNetwork IDs'
+);
