f2f364628b1369ec1ce2767891dbc3dae101dd5d
lsddreamming <jdasch92@yahoo.com>
2026-09-25 15:52:45 -0500

    Refresh cache for Block Grid polish

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'earnly-arcade-v1.1-cache-v55';const CORE_ASSETS = [
+const CACHE_NAME = 'earnly-arcade-v1.1-cache-v56';const CORE_ASSETS = [
   './',
   './index.html',
   './games.html',
