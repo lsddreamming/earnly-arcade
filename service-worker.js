435a59b6d33f005844be36fedb4480af62148220
lsddreamming <jdasch92@yahoo.com>
2026-09-25 15:04:31 -0500

    Refresh cache for Neon Maze timer rebalance

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'earnly-arcade-v1.1-cache-v51';const CORE_ASSETS = [
+const CACHE_NAME = 'earnly-arcade-v1.1-cache-v52';const CORE_ASSETS = [
   './',
   './index.html',
   './games.html',
