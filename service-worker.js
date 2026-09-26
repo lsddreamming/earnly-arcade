5c828ea332d324276bca8eadda8ccb47a7585411
lsddreamming <jdasch92@yahoo.com>
2026-09-25 19:27:40 -0500

    Refresh cache for v1.1 launch-readiness polish

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'earnly-arcade-v1.1-cache-v57';const CORE_ASSETS = [
+const CACHE_NAME = 'earnly-arcade-v1.1-cache-v58';const CORE_ASSETS = [
   './',
   './index.html',
   './games.html',
