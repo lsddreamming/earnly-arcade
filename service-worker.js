4d0d4d29860499cff2239dc57fbcf1e2ee8121cd
lsddreamming <jdasch92@yahoo.com>
2026-09-26 11:48:05 -0500

    Make Snake touch turns immediate and buffer quick corners

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'earnly-arcade-v1.1-cache-v72';const CORE_ASSETS = [
+const CACHE_NAME = 'earnly-arcade-v1.1-cache-v73';const CORE_ASSETS = [
   './compact-game-entry.css', './snake-ui.css',
   './',
   './index.html',
