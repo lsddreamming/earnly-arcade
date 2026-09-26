7cf08169da74106dbacc724d68a6042ed266f741
lsddreamming <jdasch92@yahoo.com>
2026-09-25 19:41:58 -0500

    Refresh cache for rewarded auto-resume fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'earnly-arcade-v1.1-cache-v59';const CORE_ASSETS = [
+const CACHE_NAME = 'earnly-arcade-v1.1-cache-v60';const CORE_ASSETS = [
   './',
   './index.html',
   './games.html',
